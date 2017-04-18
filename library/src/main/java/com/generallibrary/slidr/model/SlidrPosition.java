dea027913a825cfd85c3d479c85c81423aebb437
DC <dachang125@126.com>
2017-04-18 16:05:56 +0800

    添加:slidr库源码

---
+++ library/src/main/java/com/generallibrary/slidr/model/SlidrPosition.java
@@ -0,0 +1,13 @@
+package com.generallibrary.slidr.model;
+
+/**
+ * Created by r0adkll on 1/12/15.
+ */
+public enum SlidrPosition {
+    LEFT,
+    RIGHT,
+    TOP,
+    BOTTOM,
+    VERTICAL,
+    HORIZONTAL;
+}
