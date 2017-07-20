7a7ec1c0ee888555093163f357fe20d211f226fa
DC <dachang125@126.com>
2017-07-20 16:42:55 +0800

    修改;修改LibDefine的debug开关

--- library/src/main/java/com/generallibrary/base/LibDefine.java
+++ library/src/main/java/com/generallibrary/base/LibDefine.java
@@ -7,7 +7,7 @@ import android.os.Environment;
  * ..-..---.-.--..---.-...-..-....-.
  */
 public class LibDefine {
-    public static final boolean mIsDebug = true;
+    public static boolean mIsDebug = true;
     public static final boolean mIsLogToFile = false;
     public static String mPathLog = Environment.getExternalStorageDirectory().getAbsolutePath();
 }
